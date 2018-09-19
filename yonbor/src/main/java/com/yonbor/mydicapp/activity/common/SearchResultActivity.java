f5bddb9c3fbdc36d4d3f319b9504223e0e76b773
yinyb <905330018@qq.com>
2018-09-19 15:31:49 +0800

    fix

--- yonbor/src/main/java/com/yonbor/mydicapp/activity/common/SearchResultActivity.java
+++ yonbor/src/main/java/com/yonbor/mydicapp/activity/common/SearchResultActivity.java
@@ -1,13 +1,32 @@
 package com.yonbor.mydicapp.activity.common;
 
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
+import com.scwang.smartrefresh.layout.SmartRefreshLayout;
+import com.yanzhenjie.recyclerview.swipe.SwipeMenuRecyclerView;
 import com.yonbor.mydicapp.R;
 import com.yonbor.mydicapp.activity.base.BaseActivity;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class SearchResultActivity extends BaseActivity {
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+    @BindView(R.id.recyclerview)
+    SwipeMenuRecyclerView recyclerview;
+    @BindView(R.id.refreshLayout)
+    SmartRefreshLayout refreshLayout;
+    @BindView(R.id.loadLay)
+    LinearLayout loadLay;
+    @BindView(R.id.tv_toolbar_title)
+    TextView tvToolbarTitle;
 
     private String searchKey;
 
@@ -22,6 +41,18 @@ public class SearchResultActivity extends BaseActivity {
 
     @Override
     public void findView() {
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        assert actionBar != null;
+        actionBar.setDisplayShowTitleEnabled(false);
+        tvToolbarTitle.setText(searchKey);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                back();
+            }
+        });
 
+        showToast("网络架构不支持该搜索接口");
     }
 }
